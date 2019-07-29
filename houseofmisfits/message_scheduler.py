5e365a03534ec654f18ab28dd7e55844f0baaa57
Paco Rainwater <paco@heckin.io>
2019-07-29 16:15:27 -0500

    Move message scheduling to config files

--- houseofmisfits/message_scheduler.py
+++ houseofmisfits/message_scheduler.py
@@ -1,26 +1,78 @@
 import os
-from datetime import date, timedelta, datetime
+from datetime import date, timedelta, datetime, time
 
 import json
 import logging
+from pytz import timezone
 
 from houseofmisfits import Message
 
 logger = logging.getLogger(__name__)
 
 
+weekdays = {
+    0: 'Monday',
+    1: 'Tuesday',
+    2: 'Wednesday',
+    3: 'Thursday',
+    4: 'Friday',
+    5: 'Saturday',
+    6: 'Sunday'
+}
+
+
 class MessageScheduler:
     def __init__(self):
         self.messages = []
-        self.date = date.today() - timedelta(days=1)
+        self.date = date.today()
         self.rules = MessageScheduler.load_rules()
 
     def run_pending(self):
+        tz = timezone('US/Eastern')
+        if date.today() > self.date:
+            self.refresh()
+            self.date = date.today()
         for message in self.messages:
-            if datetime.now() >= message.scheduled_time:
+            if datetime.now(tz=tz) >= message.scheduled_time:
                 logger.debug("Sending message with {}: {}".format(message.webhook_name, message.message))
                 message.send()
                 self.messages.remove(message)
+
+    def refresh(self):
+        for scheduled_message in MessageScheduler.load_schedules():
+            schedule_type = scheduled_message['schedule']['type']
+            if schedule_type == 'hourly':
+                self.schedule_hourly(scheduled_message)
+            elif schedule_type == 'weekly':
+                self.schedule_weekly(scheduled_message)
+            else:
+                raise KeyError(schedule_type)
+        self.date = date.today()
+
+    def schedule_hourly(self, message):
+        tz = timezone('US/Eastern')
+        start_time = datetime.combine(date.today(), time(0, 0, 0), tzinfo=tz)
+        interval = message['schedule']['interval']
+        webhook = message['webhook']
+        unflat_text = message['message']
+        for message_time in (start_time + timedelta(hours=n) for n in range(0, 24, interval)):
+            if datetime.now(tz=tz) > message_time:
+                continue
+            new_message = Message(webhook, unflat_text, self.rules, message_time)
+            self.messages.append(new_message)
+
+    def schedule_weekly(self, message):
+        tz = timezone('US/Eastern')
+        day_of_week = weekdays[date.today().weekday()]
+        webhook = message['webhook']
+        unflat_text = message['message']
+        if day_of_week in message['schedule']['days']:
+            dt = date.today()
+            h, m = message['schedule']['time'].split(':')
+            tm = time(int(h), int(m))
+            message_time = datetime.combine(dt, tm, tzinfo=tz)
+            new_message = Message(webhook, unflat_text, self.rules, message_time)
+            self.messages.append(new_message)
 
     @staticmethod
     def load_rules():
@@ -32,9 +84,12 @@ class MessageScheduler:
                 rules.update(rule)
         return rules
 
-    def refresh(self):
-        # TODO: Move this to a config file
-        # Schedule a ping every five minutes
-        for message_time in (datetime.now() + timedelta(minutes=n) for n in range(0, 1440, 5)):
-            message = Message('aspen_general', '#wellness_reminder#', self.rules, message_time)
-            self.messages.append(message)
+    @staticmethod
+    def load_schedules():
+        schedules = []
+        schedules_dir = os.getenv('WORKSPACE') + '/schedules'
+        for file in os.listdir(schedules_dir):
+            with open(schedules_dir + '/' + file, 'r') as json_file:
+                schedule = json.load(json_file)
+                schedules += schedule
+        return schedules
