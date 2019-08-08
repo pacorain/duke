a2d9bc40413aca03f2734c8b1fee76a37be8e2c2
Paco Rainwater <paco@heckin.io>
2019-08-07 20:22:22 -0500

    Don't fail meaninglessly

--- houseofmisfits/chatbot.py
+++ houseofmisfits/chatbot.py
@@ -8,14 +8,10 @@ class Chatbot:
         logger.debug("Initializing webhooks bot")
         self.scheduler = MessageScheduler()
         self.scheduler.refresh()
-        self.i = 0
 
     def eval(self):
         """
         Check for any pending actions and perform them.
         """
         self.scheduler.run_pending()
-        self.i += 1
-        if self.i >= 15:
-            raise OSError("Example error: this is what happens when a wrench gets thrown into things.")
 
